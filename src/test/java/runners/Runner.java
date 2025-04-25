e0a0d05bd1bd74a8ccf9428496b6e3a28b350d3c
BABAYIGIT <babayigit@gmail.com>
2025-04-25 10:45:46 +0300

    first commit

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import io.cucumber.core.options.Constants;
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepdefinitions")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html")
+public class Runner {
+}
